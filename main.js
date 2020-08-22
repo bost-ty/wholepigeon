a6ab61372ed3b060e7fbc2b6d754f39575a4fd2b
Will Bostwick <wjrbostwick@gmail.com>
2020-08-21 19:29:51 -0700

    Add favicons, image-grid links JS & hover state

---
+++ main.js
@@ -0,0 +1,6 @@
+const gridItems = document.querySelectorAll(".grid-item");
+gridItems.forEach((gridItem) =>
+  gridItem.addEventListener("click", function () {
+    window.open(gridItem.dataset.post, "_blank");
+  })
+);
